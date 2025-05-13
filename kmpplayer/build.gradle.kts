55b7cdbe8213eed3f8d03c9ebe702a35c1af0f09
rufenkhokhar <rufankhokhar@gmail.com>
2025-05-13 15:57:42 +0500

    init

---
+++ kmpplayer/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    alias(libs.plugins.multiplatform)
+    alias(libs.plugins.android.library)
+    id("convention.publication")
+}
+
+group = "io.github.rufenkhokhar"
+version = "1.0.0"
+
+kotlin {
+    jvmToolchain(21)
+
+    androidTarget { publishLibraryVariants("release") }
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    sourceSets {
+        commonMain.dependencies {
+        }
+
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+        }
+
+    }
+
+    //https://kotlinlang.org/docs/native-objc-interop.html#export-of-kdoc-comments-to-generated-objective-c-headers
+    targets.withType<org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget> {
+        compilations["main"].compileTaskProvider.configure {
+            compilerOptions {
+                freeCompilerArgs.add("-Xexport-kdoc")
+            }
+        }
+    }
+
+}
+
+android {
+    namespace = "io.github.rufenkhokhar"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 21
+    }
+}
